acd0acc565b71c5449dda27ce96aa8631ab2777f
Shawn-Nevermore <shawnxu2016sg@gmail.com>
2019-09-08 23:19:52 +0800

    添加vue-router和vuex，实现parent和child之间的通信

---
+++ src/components/child.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <span>我是子组件</span>
+    <div>来自父组件的message： {{ msg }}</div>
+    <div>
+      <button @click="sendMsg2Parent">向父组件传递信息</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "child",
+  data() {
+    return {
+      msg2Parent: "爹，儿子给您发微信了"
+    };
+  },
+  props: {
+    msg: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    sendMsg2Parent() {
+      this.$emit("handle", this.msg2Parent);
+    }
+  }
+};
+</script>
+
+<style lang="stylus" scoped></style>
---
+++ src/components/parent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <span>我是父组件</span>
+    <p>儿子发来的微信：{{ data }}</p>
+    <hr />
+    <child :msg="message" @handle="getMsgFromChild"></child>
+  </div>
+</template>
+
+<script>
+import child from "./child";
+export default {
+  name: "parent",
+  data() {
+    return {
+      message: "我是你爸爸",
+      data: ""
+    };
+  },
+  components: {
+    child
+  },
+  methods: {
+    getMsgFromChild(value) {
+      this.data = value;
+    }
+  }
+};
+</script>
+
+<style lang="stylus" scoped></style>
